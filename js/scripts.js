2e1c6afaf2a366e3d176fe6ae145f5b189bbeb9a
Jeremy Kale Padot <jeremy@padot.us>
2020-01-14 22:22:02 -0800

    add spacing for phone

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 
   $(".toaster").click(function(){
     var result = alert("L-lets take a bath oni-chan")
-    prompt("w-would you like to take a bath? (　･ω･)☞ y/n? ")
+    prompt("w-would you like to take a bath?       (　･ω･)☞ y/n? ")
    alert(" (´×ω×`) you have died instantly (´×ω×`) ")
  });
 
