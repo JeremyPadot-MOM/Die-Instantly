230eb3f3f40323520c843817f7985508d267942c
Jeremy Kale Padot <jeremy@padot.us>
2020-01-14 22:16:10 -0800

    first draft

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,10 @@
 $(document).ready(function() {
 
 
-
+  $(".toaster").click(function(){
+    var result = alert("L-lets take a bath oni-chan")
+    prompt("w-would you like to take a bath? (　･ω･)☞ y/n? ")
+   alert(" (´×ω×`) you have died instantly (´×ω×`) ")
+ });
 
 });
