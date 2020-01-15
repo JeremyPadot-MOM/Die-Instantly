5ece3bed153bd20b8227c3395721303a2c12b5ab
Jeremy Kale Padot <jeremy@padot.us>
2020-01-14 22:35:49 -0800

    senpai

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
   $(".toaster").click(function(){
     var result = alert("L-lets take a bath oni-chan")
     prompt("w-would you like to take a bath?       (　･ω･)☞ y/n? ")
-   alert(" (´×ω×`) you have died instantly (´×ω×`) ")
+   alert(" (´×ω×`) you have died instantly. s-sorry senpai (´×ω×`) ")
  });
 
 });
